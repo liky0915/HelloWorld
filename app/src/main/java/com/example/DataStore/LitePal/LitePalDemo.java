1e114cbf7c2b2d12e76070192e07ca52cc37745e
Lester Ding <polo83@qq.com>
2017-08-01 12:17:07 +0800

    20170801_update

--- app/src/main/java/com/example/DataStore/LitePal/LitePalDemo.java
+++ app/src/main/java/com/example/DataStore/LitePal/LitePalDemo.java
@@ -1,12 +1,17 @@
 package com.example.DataStore.LitePal;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.helloworld.R;
 
 import org.litepal.LitePal;
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
 
 /**
  * Created by lester.ding on 7/28/2017.
@@ -14,10 +19,13 @@ import org.litepal.LitePal;
 
 public class LitePalDemo extends AppCompatActivity {
 
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.litepal_demo_layout);
+        //初始化
+        text = (TextView) findViewById(R.id.query_result);
     }
 
     public void doClick(View v){
@@ -56,9 +64,56 @@ public class LitePalDemo extends AppCompatActivity {
                 break;
 
             case R.id.delete_data:
+                //删除方式一
+                Book book3 = new Book();
+                book3.setAuthor("山歌");
+                book3.setName("十里春风不如你");
+                book3.setPages(211);
+                book3.setPrice(11.2);
+                book3.setPress("中国湖南出版社");
+                book3.save();
+                book3.delete();
+                //删除方式二
+                Book book4 = new Book();
+                book4.setAuthor("山哥");
+                book4.setName("十里春风不如你");
+                book4.setPages(211);
+                book4.setPrice(11.2);
+                book4.setPress("中国湖南出版社");
+                book4.save();
+                DataSupport.deleteAll(Book.class, "author=?", "山哥");//第一个参数指定要删哪张表，第二和三个参数组成过滤条件
                 break;
 
             case R.id.query_data:
+                //查询方式一：使用LitePal查询API（推荐）
+                //Book firstBook = DataSupport.findFirst(Book.class);     //查询表中第一条数据
+                //Book lastBook = DataSupport.findLast(Book.class);       //查询表中最后条数据
+                //List<Book> allBooks = DataSupport.findAll(Book.class);  //查询表中所有数据
+                //组合条件查询
+                List<Book> selBooks = DataSupport.select("author", "name")
+                        .where("author=?", "小陶子")
+                        .order("pages desc")
+                        .find(Book.class);
+                //读取查询结果
+                StringBuilder content = new StringBuilder();
+                for(Book b: selBooks){
+                    content.append("作者："+b.getAuthor()+", 书名："+b.getName()+"\n");
+                }
+
+                //查询方式二：原生SQL查询
+                Cursor c = DataSupport.findBySQL("select author, name from Book where author=?", "动感小辣鸡");
+                if(c.moveToFirst()){
+                    do {
+                        //从游标中提取数据
+                        String author = "作者："+c.getString(c.getColumnIndex("author"));
+                        String name = "书名："+c.getString(c.getColumnIndex("name"));
+                        content.append(author+", "+name+"\n");
+                    } while (c.moveToNext());
+                }
+                c.close(); //关闭游标
+
+                //显示最终查询结果
+                text.setText(content.toString());
                 break;
         }
     }
